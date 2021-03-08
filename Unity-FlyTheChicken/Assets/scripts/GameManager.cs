d9ee687033c80d04b5b924b2e32af65d4f79bbb9
DESKTOP-R9SJI8K\Giant <shaif.dihan@gmail.com>
2021-03-08 17:23:13 +0600

    Parallax

---
+++ Unity-FlyTheChicken/Assets/scripts/GameManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public delegate void GameDelegate();
+    public static event GameDelegate OnGameStarted;
+    public static event GameDelegate OnGameOverConfirmed;
+
+    public static GameManager Instance;
+    public GameObject startPage;
+    public GameObject gameOverPage;
+    public GameObject countDownPage;
+    public Text scoreText;
+
+    enum PageState {
+        None,
+        Start,
+        GameOver,
+        CountDown
+    }
+
+    int score = 0;
+    bool gameOver = false;
+    
+    public bool GameOver { get { return gameOver; } }
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void OnEnable(){
+        CountdownText.OnCountdownFinished += OnCountdownFinished;
+        TapController.OnPlayerDied += OnPlayerDied;
+        TapController.OnPlayerScored += OnPlayerScored;
+
+    }
+
+    void OnDisable(){
+        CountdownText.OnCountdownFinished -= OnCountdownFinished;
+        TapController.OnPlayerDied -= OnPlayerDied;
+        TapController.OnPlayerScored -= OnPlayerScored;
+
+    }
+
+    void OnCountdownFinished(){
+        SetPageState(PageState.None);
+        OnGameStarted();
+        score=0;
+        gameOver = false;
+    }
+
+    void OnPlayerDied(){
+        gameOver = true;
+        int savedScore = PlayerPrefs.GetInt("HighScore");
+        if(score > savedScore)
+        {
+            PlayerPrefs.SetInt("Highscore",score);
+        }
+        SetPageState(PageState.GameOver);
+    }
+
+    void OnPlayerScored(){
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+    void SetPageState(PageState state) 
+    {
+        switch(state){
+            case PageState.None:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countDownPage.SetActive(false);
+            break;
+
+            case PageState.Start:
+                startPage.SetActive(true);
+                gameOverPage.SetActive(false);
+                countDownPage.SetActive(false);
+            break;
+
+            case PageState.GameOver:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(true);
+                countDownPage.SetActive(true);
+            break;
+
+            case PageState.CountDown:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countDownPage.SetActive(true);
+            break;
+        }
+    }
+
+    public void ConfirmGameOver(){
+        OnGameOverConfirmed();
+        scoreText.text= "0";
+        SetPageState(PageState.Start);
+    }
+
+    public void StartGame(){
+        SetPageState(PageState.CountDown);
+    }
+}
---
+++ Unity-FlyTheChicken/Assets/scripts/TapController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))] 
+
+public class TapController : MonoBehaviour
+{   
+    public delegate void PlayerDelegate();
+    public static event PlayerDelegate OnPlayerDied;
+    public static event PlayerDelegate OnPlayerScored;
+
+    public float tapForce = 10;
+    public float tiltSmooth = 5;
+    public Vector3 startPos;
+
+    Rigidbody2D rigidbody1;
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+
+
+    void Start()
+    {
+        rigidbody1 = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0,0,-90);
+        forwardRotation = Quaternion.Euler(0,0,35);
+    }
+
+    void OnEnable(){
+        GameManager.OnGameStarted += OnGameStarted;
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
+    }
+
+    void OnDisable(){
+        GameManager.OnGameStarted -= OnGameStarted;
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+    }
+
+    void OnGameStarted(){
+        rigidbody1.velocity = Vector3.zero;
+        rigidbody1.simulated = true;
+    }
+
+    void OnGameOverConfirmed(){
+        transform.localPosition = startPos;
+        transform.rotation = Quaternion.identity;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = forwardRotation;
+            rigidbody1.velocity = Vector3.zero;
+            rigidbody1.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+
+    }
+
+    void onTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "ScoreZone") 
+        { 
+            OnPlayerScored();
+        
+        }
+        if (col.gameObject.tag == "DeadZone") {
+            rigidbody1.simulated = false;
+            OnPlayerDied();
+        }
+
+    }
+}
